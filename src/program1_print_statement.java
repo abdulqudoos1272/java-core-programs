a105ce946f0225be1d229cb1425c5bb829035520
abdulqudoos9121 <aq31302890@gmail.com>
2025-05-09 21:39:14 +0500

    practice print statement

---
+++ src/program1_print_statement.java
@@ -0,0 +1,9 @@
+public class program1_print_statement {
+    public static void main(String []arg){
+        System.out.println("Hello World");
+        System.out.println("My name is : Abdul Qudoos");
+        System.out.println("My age is : 23 in 2025");
+        System.out.println("Education : Currently Enrollrd in BSCS 2022-26");
+        System.out.println("City is : Islamabad");
+    }
+};
