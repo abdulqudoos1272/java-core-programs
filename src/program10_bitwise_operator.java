3fb9c6940d0a62af85343c67979fc6b4c15cf3b2
abdulqudoos9121 <aq31302890@gmail.com>
2025-05-09 22:09:01 +0500

    practice with bitwise operator

---
+++ src/program10_bitwise_operator.java
@@ -0,0 +1,11 @@
+public class program10_bitwise_operator {
+    public static void main(String []arg){
+        int p=6,q=4;
+        System.out.println("Value of p is : "+p);
+        System.out.println("Value of q is : "+q);
+        System.out.println("Bitwise and is : "+(p&q));
+        System.out.println("Bitwise or is : "+(p|q));
+        System.out.println("Bitwise xor is : "+(p^q));
+        System.out.println("Bitwise not is : "+(~p));
+    }
+}
