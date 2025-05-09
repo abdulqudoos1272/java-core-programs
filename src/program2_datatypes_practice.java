207a1674ed2288e634609b6848e008b7f5ebef30
abdulqudoos9121 <aq31302890@gmail.com>
2025-05-09 21:41:49 +0500

    practice with java datatypes

---
+++ src/program2_datatypes_practice.java
@@ -0,0 +1,16 @@
+public class program2_datatypes_practice {
+    public static void main(String []arg){
+        int a=10;
+        float b=2.3f;
+        double c=23.56d;
+        long d=2345l;
+        char e='a';
+        String f="Hello World";
+        System.out.println("Use Integer Data Type : "+a);
+        System.out.println("Use Float Data Type : "+b);
+        System.out.println("Use Double Data Type : "+c);
+        System.out.println("Use Long Data Type : "+d);
+        System.out.println("Use Char Data Type : "+e);
+        System.out.println("Use String Data Type : "+f);
+    }
+};
