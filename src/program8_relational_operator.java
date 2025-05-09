5f999795ab7b0e864b60e41882ea983e104d7cd6
abdulqudoos9121 <aq31302890@gmail.com>
2025-05-09 22:04:12 +0500

    practice with relational operator

---
+++ src/program8_relational_operator.java
@@ -0,0 +1,42 @@
+public class program8_relational_operator {
+    public static void main(String []arg){
+        int c=45,d=34;
+        System.out.println("Value of c is "+c+" and d is "+d);
+        if(c==d){
+            System.out.println("c is equal to d");
+        }
+        else{
+            System.out.println("c is equal to d");
+        }
+        if(c<d){
+            System.out.println("c is Less than to d");
+        }
+        else{
+            System.out.println("c is not less than to d");
+        }
+        if(c>d){
+            System.out.println("c is grater than to d");
+        }
+        else{
+            System.out.println("c is not greater than to d");
+        }
+        if(c<=d){
+            System.out.println("c is less than and equal to d");
+        }
+        else{
+            System.out.println("c is not less than and equal to d");
+        }
+        if(c>=d){
+            System.out.println("c is greater than and equal to d");
+        }
+        else{
+            System.out.println("c is not greater than and equal to d");
+        }
+        if(c!=d){
+            System.out.println("c is not equal to d");
+        }
+        else{
+            System.out.println("c is  equal to d");
+        }
+    }
+}
