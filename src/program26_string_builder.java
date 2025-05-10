f132ebaa5835abd86b0bbc523c966d5ae93447c4
abdulqudoos9121 <aq31302890@gmail.com>
2025-05-10 17:31:04 +0500

    practice with built in method of string builder

--- src/program26_string_builder.java
+++ src/program26_string_builder.java
@@ -2,7 +2,15 @@ public class program26_string_builder {
     public static void main(String []arg){
         StringBuilder s1=new StringBuilder("Hello");
         System.out.println(s1);
-        s1.append(" World");
+        s1.append(" World");     //use append method instead of concatenation
         System.out.println(s1);
+        s1.insert(5," :"); //use insert method
+        System.out.println(s1);
+        s1.setCharAt(6,'!'); //set char at index 5
+        System.out.println(s1);
+        System.out.println("Length of s1 is : "+s1.length()); //find length of s1
+        s1.deleteCharAt(6); //delete char at index 6
+        System.out.println(s1);
+
     }
 }
