bb34ade262c9a4bab42503e1a8f6f572023d4089
abdulqudoos9121 <aq31302890@gmail.com>
2025-05-10 17:22:02 +0500

    pracice with built in function of string

--- src/program25_string.java
+++ src/program25_string.java
@@ -2,6 +2,20 @@ public class program25_string {
     public static void main(String []arg){
         String info="Name : Abdul Qudoos";
         System.out.println(info);
-
+        info=info+", Gender: Male"; // Use commConcatenation
+        System.out.println(info);
+        System.out.println("Length of String is : "+info.length()); //Find Length of String
+        System.out.println("Char at index 4 : "+info.charAt(4));    //Get char at index 4
+        //Compare the Strings
+        String string1="Hello",string2="World";
+        if((string1.compareTo(string2))>0){
+            System.out.println("String 1 is greater");
+        }
+        else if((string1.compareTo(string2))<0){
+            System.out.println("String 2 is greater");
+        }
+        else{
+            System.out.println("Both are equal");
+        }
     }
 }
