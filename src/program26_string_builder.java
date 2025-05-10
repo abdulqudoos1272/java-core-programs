64f1e9f5a99002d3d86ea9c27f0dc7ed2239c7a2
abdulqudoos9121 <aq31302890@gmail.com>
2025-05-10 17:25:33 +0500

    practice with string bulider

---
+++ src/program26_string_builder.java
@@ -0,0 +1,8 @@
+public class program26_string_builder {
+    public static void main(String []arg){
+        StringBuilder s1=new StringBuilder("Hello");
+        System.out.println(s1);
+        s1.append(" World");
+        System.out.println(s1);
+    }
+}
